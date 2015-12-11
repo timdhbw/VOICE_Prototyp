56fdeafe1f735762b0b4a8301008f9419d9d4ea0
Nico Becker <nico.becker1@gmail.com>
2015-12-11 11:30:41 +0100

    Package updated

---
+++ src/dhbw/ai13/ann/PrototypeTest.java
@@ -0,0 +1,29 @@
+package dhbw.ai13.ann;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import dhbw.ai13.speech.detection.Vector13D;
+
+public class PrototypeTest {
+
+	private ArrayList<Vector13D> mfccs;
+	private SpeakerNet speakerNet;
+	
+	public PrototypeTest(){
+		mfccs = new ArrayList<>();
+	}
+	public void init() throws NumberFormatException, IOException {
+		
+	}
+	
+	public static void main(String[] args) {
+		PrototypeTest net = new PrototypeTest();
+		try {
+			net.init();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} 
+	}
+
+}
