d3599a61aea9a72cfc3ac1674ead42873b9a84e3
Tim <tim.tiede@freenet.de>
2016-01-07 18:57:03 +0000

    Testpacket ersellt

---
+++ src/dhbw/ai13/VOICETest/TestClass.java
@@ -0,0 +1,71 @@
+package dhbw.ai13.VOICETest;
+
+import java.util.ArrayList;
+
+import dhbw.ai13.bayesClassificator.naiveBayes.Algorithm;
+import dhbw.ai13.bayesClassificator.naiveBayes.Result;
+import dhbw.ai13.bayesClassificator.trainer.NaiveBayesTrainer;
+import dhbw.ai13.spectrogram.SpectrogrammErsteller;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		SpectrogrammErsteller creater = new SpectrogrammErsteller(1024, 0, "resources/A_Eric_Zenker_01.wav");
+		NaiveBayesTrainer nbt = new NaiveBayesTrainer(10000, 256, 10, 0); 
+		String[] st = {"a"};
+		nbt.getDatabase().setNameOfRow(st);
+		double[][] d = change1(creater);
+		
+		nbt.trainDatabase(d, "a");
+		 creater = new SpectrogrammErsteller(1024, 0, "resources/A_Eric_Zenker_02.wav");
+		d = change1(creater);
+		
+		nbt.trainDatabase(d, "a");
+		System.out.println("Training beendet");
+//		
+		d = changer2(d);
+		
+		Algorithm alg = new Algorithm(nbt.getDatabase(), d);
+		ArrayList<Result> res = alg.getBestResults();
+		System.out.println("Results berechnet");
+		for(int i=0;i<res.size();i++){
+			System.out.println(res.get(i).toString());
+		}
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	private static double[][] change1(SpectrogrammErsteller creater ){
+		double[][] d = new double[10][32];
+		int t = 0;
+		double help = 0;
+		for(int i=0;i<d.length;i++){
+			for(int j=0;j<256;j++){
+				help = help + creater.getSpectrogramData()[i][j];
+				if(t==8){
+					d[i][j/8] = help;
+					t = t+1;
+					t=0;
+					help=0;
+				}
+			}
+		}
+		return d;
+	}
+	
+	private static double[][] changer2(double[][] d){
+		for(int i=0;i<d.length;i++){
+			for(int j=0;j<d[0].length;j++){
+				d[i][j] = d[i][j]/100;
+			}
+		}
+		return d;
+	}
+
+}
