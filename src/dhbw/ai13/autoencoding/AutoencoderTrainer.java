ae867c2c91c21970db1e2bb3f129f7b21476a7b7
Eric Zenker <eric.zenker@web.de>
2016-04-18 14:47:47 +0200

    Zwischenupdate Autoencoder

---
+++ src/dhbw/ai13/autoencoding/AutoencoderTrainer.java
@@ -0,0 +1,7 @@
+package dhbw.ai13.autoencoding;
+
+/**
+ * Created by GomaTa on 18.04.2016.
+ */
+public class AutoencoderTrainer {
+}
---
+++ src/dhbw/ai13/autoencoding/framework/AudiofileHandler.java
@@ -0,0 +1,7 @@
+package dhbw.ai13.autoencoding.framework;
+
+/**
+ * Created by GomaTa on 10.04.2016.
+ */
+public class AudiofileHandler {
+}
